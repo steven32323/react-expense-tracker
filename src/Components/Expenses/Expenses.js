983057d90a9b771951537bc39cd8f77535531de0
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-03-14 17:17:02 +0000

    commit

---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Expenses.css';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter(
+    expense => expense.date.getFullYear() === parseInt(filteredYear)
+  );
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChange={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/Components/NewExpense/ExpenseForm.js
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = props => {
+  // setting states
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  // handle title changes
+  const titleChangeHandler = e => {
+    setEnteredTitle(e.target.value);
+  };
+  // handle amount changes
+  const amountChangeHandler = e => {
+    setEnteredAmount(e.target.value);
+  };
+  // handle date changes
+  const dateChangeHandler = e => {
+    setEnteredDate(e.target.value);
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: +enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  const [formOpen, setFormOpen] = useState(false);
+  const newFormHandler = () => {
+    setFormOpen(true);
+  };
+  const cancelFormHandler = () => {
+    setFormOpen(false);
+  };
+
+  return (
+    <>
+      {formOpen === false ? (
+        <button onClick={newFormHandler}>Add New Expense</button>
+      ) : (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                min="0.01"
+                step="0.01"
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+              />
+            </div>
+          </div>
+          <div className="new-expense__actions">
+            <button type="button " onClick={cancelFormHandler}>
+              Cancel
+            </button>
+            <button type="submit">Add Expense</button>
+          </div>
+        </form>
+      )}
+    </>
+  );
+};
+export default ExpenseForm;
